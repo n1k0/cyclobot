3b935ba97ad1f8922ea2af0fd01134c4cde6b4b1
Nicolas Perriault <nicolas@perriault.net>
2024-08-02 19:20:04 +0200

    Add image generation.

--- index.js
+++ index.js
@@ -24,9 +24,19 @@ const client = createRestAPIClient({
 const openai = new OpenAI();
 
 async function createAndSendToot() {
+  // Toot text
+  const tootText = await generateTootText();
+  // Image
+  const imageUrl = await generateTootImage(tootText);
+  const remoteFile = await fetch(imageUrl);
+  const attachment = await client.v2.media.create({
+    file: await remoteFile.blob(),
+    description: "Illustration générée des technologies mentionnées dans le pouet.",
+  });
   const { url } = await createToot({
-    status: await generateToot(),
+    status: tootText,
     visibility: process.env.VISIBILITY || "direct",
+    mediaIds: [attachment.id],
   });
   console.log(`New toot posted: ${url}`);
 }
@@ -47,22 +57,30 @@ async function createToot(params, retries = 3, backoff = 500) {
   }
 }
 
-async function generateToot() {
-  const prompt = fs
-    .readFileSync("prompt.txt")
-    .toString("utf8")
-    .replaceAll("\n", "")
-    .replaceAll("  ", " ");
+async function generateTootImage(toot) {
+  const imagePrompt = `Est-ce que tu peux me produire une image d'illustration pas trop science-fiction et plutôt crédible et réaliste pour ce message que tu as généré : “${toot}”. Surtout, évite ABSOLUMENT et IMPÉRATIVEMENT de positionner le moindre texte sur l'image générée.`;
+  const response = await openai.images.generate({
+    model: "dall-e-3",
+    prompt: imagePrompt,
+    n: 1,
+    quality: "hd",
+    size: "1792x1024",
+  });
+  return response.data[0].url;
+}
+
+async function generateTootText() {
+  const prompt = fs.readFileSync("prompt.txt").toString("utf8");
   console.debug("Prompt", prompt);
   const response = await openai.chat.completions.create({
     model: "gpt-4o",
     messages: [{ role: "system", content: prompt }],
   });
-  const tootContent = response.choices[0].message.content;
+  const tootContent = response.choices[0].message.content.replaceAll("*", "");
   console.debug("Toot content", tootContent);
   if (tootContent.length > MAX_TOOT_LENGTH) {
     console.warn(`Toot length exceeds ${MAX_TOOT_LENGTH}c., requesting a new one.`);
-    return await generateToot();
+    return await generateTootText();
   } else {
     return tootContent;
   }
